f1d1d9fb42f2611605cad35d3d326fefa9e1e9ba
Deepjyoti <deepjyotiboruahdeep@gmail.com>
2026-01-08 19:01:19 +0530

    Fix 404 error by removing base path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // The 'base' line has been removed to fix the Vercel 404 error
 })
